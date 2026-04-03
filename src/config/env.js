0f1aad0a7cc2db6dc8cae374351f0bfd97488639
trangnt <truongtrang.it.spkt@gmail.com>
2026-04-03 16:01:49 +0700

    Update env

--- src/config/env.js
+++ src/config/env.js
@@ -16,7 +16,7 @@ const env = {
   nodeEnv: process.env.NODE_ENV || "development",
   port: Number(process.env.PORT) || 4000,
   mongoUri: process.env.MONGO_URI || "mongodb://127.0.0.1:27017",
-  clientUrl: process.env.CLIENT_URL || "http://localhost:5173",
+  clientUrl: process.env.CLIENT_URL || "http://localhost:3000",
   accessTokenTtlMinutes: Number(process.env.ACCESS_TOKEN_TTL_MINUTES) || 15,
   refreshTokenTtlDays: Number(process.env.REFRESH_TOKEN_TTL_DAYS) || 30,
   passwordResetTokenTtlMinutes:
