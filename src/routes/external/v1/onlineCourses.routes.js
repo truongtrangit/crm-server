b5753c42e215cfec7ac4b867703eea36700c7a51
trangnt <truongtrang.it.spkt@gmail.com>
2026-06-29 19:35:52 +0700

    feat: add optional authentication middleware to online course routes

--- src/routes/external/v1/onlineCourses.routes.js
+++ src/routes/external/v1/onlineCourses.routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const CourseOnlineController = require("../../../modules/course/courseOnline/courseOnline.controller");
 const CourseConfigController = require("../../../modules/course/courseConfig/courseConfig.controller");
+const {
+  optionalBotvnAuthenticateRequest,
+} = require("../../../core/middleware/externalAuth");
 
 const router = express.Router();
 
-router.get("/", CourseOnlineController.getExternalCourses);
+router.get(
+  "/",
+  optionalBotvnAuthenticateRequest,
+  CourseOnlineController.getExternalCourses
+);
 router.get("/categories", CourseConfigController.getCategories);
-router.get("/:identifier", CourseOnlineController.getCourseByIdentifier);
+router.get(
+  "/:identifier",
+  optionalBotvnAuthenticateRequest,
+  CourseOnlineController.getCourseByIdentifier
+);
 
 module.exports = router;
